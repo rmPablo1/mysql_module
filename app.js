d617e00ec04812bc69dcd3cf62cbfe1a1d72af7e
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-05-25 14:17:19 +0200

    finished

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ const authRoutes = require('./routes/auth');
 
 /* const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {flags: "a"})
  */
-app.use(helmet())
 app.use(compression())
 /* app.use(morgan("combined", {stream: accessLogStream})) */
 
